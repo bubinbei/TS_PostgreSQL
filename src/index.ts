3720e8b1cd199ec2dd0c48a4a70b667d12a5f085
bubinbei <bubinbei@yandex.ru>
2026-03-11 10:53:00 +0400

    crud

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,58 @@ app.get('/users', async (c) => {
 })
 
 // get by id
-// 
+// withPrisma подключен глобально, передавать не обязательно
+// app.get('/users/:id', withPrisma, async (c) => {
+app.get('/users/:id', async (c) => {
+  const prisma = c.get('prisma')
+  const id = c.req.param('id')
+  const user = await prisma.user.findUnique({
+    where: { id }
+  })
+  if (!user) {
+    return c.json({ error: 'User not found' }, 404)
+  }
+  return c.json(user)
+})
+
 // create
-// 
+// withPrisma подключен глобально, передавать не обязательно
+
+app.post('/users', async (c) => {
+  const prisma = c.get('prisma')
+  const { mail, password, username } = await c.req.json();
+  const id = c.req.param('id')
+  const user = await prisma.user.create({
+    data: { 
+      mail, 
+      password, 
+      username 
+    }
+  })
+  return c.json(user, 201)
+})
+
 // update
-// 
+// update
+app.put('/users/:id', async (c) => {
+  const prisma = c.get('prisma')
+  const id = c.req.param('id')
+  const { mail, password, username } = await c.req.json()
+  const user = await prisma.user.update({
+    where: { id },
+    data: { mail, password, username }
+  })
+  return c.json(user)
+})
 // delete
-// 
+app.delete('/users/:id', async (c) => {
+  const prisma = c.get('prisma')
+  const id = c.req.param('id')
+  const user = await prisma.user.delete({
+    where: { id }
+  })
+  return c.json(user)
+})
 
 serve({
   fetch: app.fetch,
